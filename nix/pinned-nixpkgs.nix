bb9e37b6c8f68fe9d39b79a5b90505edf80d1fb1
Jonathan Lorimer <jonathan_lorimer@mac.com>
2021-04-20 00:11:21 -0400

    feat: turbo-frames
    remove node_modules
    
    remove artefacts
    
    remove client.js

--- nix/pinned-nixpkgs.nix
+++ nix/pinned-nixpkgs.nix
@@ -1,8 +1,5 @@
 builtins.fetchGit {
-  # Descriptive name to make the store path easier to identify
   url = "https://github.com/nixos/nixpkgs/";
-  # Commit hash for nixos-unstable as of 2018-09-12
-  # `git ls-remote https://github.com/nixos/nixpkgs nixos-unstable`
   ref = "refs/heads/nixos-unstable";
   rev = "f5e8bdd07d1afaabf6b37afc5497b1e498b8046f";
 }
