a5e69c58aadefbd24bf84f3f258de36a390374eb
Jonathan Lorimer <jonathan_lorimer@mac.com>
2021-04-28 20:44:25 -0400

    cleanup repo

--- demo/hotwire-client/hotwire.js
+++ demo/hotwire-client/hotwire.js
@@ -9,14 +9,14 @@ document.addEventListener("turbo:load", function(e) {
   console.groupEnd()
 })
 
-const ws = new WebSocket("ws://localhost:80");
+const ws = new WebSocket("ws://localhost:8081");
 const streamSource =
   {
     addEventListener: (_, listener) => {
-      ws.onmessage = (evt) => {console.log("onmessage", evt); listener(evt)}
+      ws.onmessage = listener
     },
     removeEventListener: (_, listener) => {
-      ws.onclose = (evt) => {console.log("onclose", evt); listener(evt)}
+      ws.onclose = listener
     }
   }
 
